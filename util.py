e65696d971ecaa7b71358e54bf4397358dc90b5f
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2022-05-27 13:51:34 +0300

    Add catchArg

--- util.py
+++ util.py
@@ -1,7 +1,52 @@
 from math import floor
 from typing import Union, Any
+from sys import argv
 
 
+def indexExists(l: list[Any], id: int) -> bool:
+  return 0 <= id < len(l)
+
+
+# Catch argument from command-line execution
+def catchArg(arg: str) -> Any:
+  if len(argv) < 2:
+    return False
+  
+  try:
+    normArgv = list(map(lambda arg: arg.lower(), argv))
+    argId = normArgv.index(arg)
+    if indexExists(normArgv, argId + 1):
+      return normArgv[argId + 1]
+    else:
+      return True
+  except:
+    return False
+
+
+def timeFmt(
+    hh: Union[int, None] = None,
+    mm: Union[int, None] = None,
+    ss: Union[int, None] = None,
+    ms: Union[int, None] = None,
+    ns: Union[float, None] = None
+  ) -> str:
+  if ns is None:
+    if hh is None or mm is None or ss is None:
+      raise TypeError("One of the args: hh, mm or ss is None")
+
+    if ms is None:
+      return "{:02d}:{:02d}:{:02d}".format(hh, mm, ss)
+    else:
+      return "{:02d}:{:02d}:{:02d}.{:03d}".format(hh, mm, ss, ms)
+  else:
+    return "{:02.2f} ns".format(round(ns, 2))
+
+
+# Used to map dict item to tuple of specified args
+# Example:
+#
+# d = { "a": 1, "b": 2 }
+# (a, b) = pluck(d, "a", "b")
 def pluck(dict: dict[str, Any], *args: str):
   return (dict[arg] for arg in args)
 
@@ -13,7 +58,7 @@ def secondsToTime(seconds: Union[int, float] = 0) -> str:
   mm = int(floor(seconds / 60) % 60)
   hh = int(floor(((seconds / 60) / 60)))
 
-  return "{:02d}:{:02d}:{:02d}".format(hh, mm, ss)
+  return timeFmt(hh, mm, ss)
 
 
 def msToTime(milliseconds: Union[int, float] = 0) -> str:
@@ -24,10 +69,9 @@ def msToTime(milliseconds: Union[int, float] = 0) -> str:
   mm = int(floor(milliseconds / 1000 / 60) % 60)
   hh = int(floor(milliseconds / 1000 / 60 / 60) % 60)
 
-  return "{:02d}:{:02d}:{:02d}.{:03d}".format(hh, mm, ss, ms)
+  return timeFmt(hh, mm, ss, ms)
 
 
 def msToNs(milliseconds: Union[int, float] = 0) -> str:
-  nanoseconds = round(milliseconds * 1000, 2)
-
-  return "{:02.2f} ns".format(nanoseconds)
+  nanoseconds = milliseconds * 1000
+  return timeFmt(ns=nanoseconds)
